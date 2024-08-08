1be71bf57c47731759aa749913dbbdbad0f672cb
Nikhil Sharma <67474105+Confused-code@users.noreply.github.com>
2024-08-09 01:47:30 +0530

    Add extra credit feature opacity

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ const clearBoard = document.querySelector('clearBoard');
 
 let toColor = false;
 
-sketchBoard.addEventListener('mouseover', colorSketchUnit);
+// sketchBoard.addEventListener('mouseover', colorSketchUnit);
 resetGenerateButton.addEventListener('click', getSketchUnitRows);
 colorButton.addEventListener('click', () => toColor=true);
+blackColorButton.addEventListener('click', ()=>toColor=false);
 
 createBoard();
 
@@ -21,8 +22,11 @@ function createBoard(n=16) {
     for(let i=0; i<numberOFsketchUnits; i++) {
         const sketchUnit = document.createElement("div");
         sketchUnit.classList.add("sketchUnit");
+        sketchUnit.style.backgroundColor == "#fff";
         sketchUnit.style.width = `${sketchUnitSize}px`;
         sketchUnit.style.height = `${sketchUnitSize}px`;
+
+        sketchUnit.addEventListener('mouseover', colorSketchUnit)
 
         sketchBoard.appendChild(sketchUnit);
     }
@@ -40,10 +44,36 @@ function getSketchUnitRows() {
 }
 
 function colorSketchUnit(e) {
-    if(!toColor)
-        e.target.classList.add("colorItBlack");
-    else {
-        let color = Math.floor(Math.random()*361);
-        e.target.style.backgroundColor = "hsl("+color+" 100%, 50%)";
+    if(!toColor) {
+        const targetUnit = e.target;
+        if(targetUnit.style.backgroundColor == "" ||
+            targetUnit.style.backgroundColor !== 'rgb(110, 172, 218)'
+        ) {
+            targetUnit.style.backgroundColor = "#6EACDA";
+            e.target.style.opacity = "0.1";
+        } else {
+            if(isFilled(e)) {
+                if(+(e.target.style.opacity<1))
+                    e.target.style.opacity = `${+(e.target.style.opacity) + 0.1}`;
+            }
+        }
     }
+    else {
+        if(e.target.style.backgroundColor == "rgb(110, 172, 218)" || 
+            e.target.style.backgroundColor == ''
+        ) {
+            let color = Math.floor(Math.random()*361);
+            e.target.style.backgroundColor = "hsl("+color+" 100%, 50%)";
+            e.target.style.opacity = "0.1";
+        }
+        else {
+            if(+(e.target.style.opacity<1))
+                e.target.style.opacity = `${+(e.target.style.opacity) + 0.1}`;
+        }
+    }
+}
+
+function isFilled(e) {
+    if (e.target.style.opacity === "") return false;
+    else return true;
 }
